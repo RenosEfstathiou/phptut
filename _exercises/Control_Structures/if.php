d59a31f0c1ad7a76dd7fa56a9c81b644ceff9189
Renos Efstathiou <it164826@it.teithe.gr>
2020-12-11 00:32:38 +0200

    A simple file structure change

---
+++ _exercises/Control_Structures/if.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If Statement</title>
+</head>
+
+<body>
+
+    <?php
+
+    if (3 > 10) {
+        echo 'three is less that ten';
+    } elseif (4 > 3) {
+        echo '4 is greater than three';
+    } else {
+        echo 'none of the above';
+    }
+    ?>
+</body>
+
+</html>
